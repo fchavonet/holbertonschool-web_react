59df7abff8a608202acb23aebd834062854b198f
fchavonet <fchavonet@mail.fr>
2025-07-04 15:40:18 +0200

    Create isDirector type predicate and executeWork function.

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -44,3 +44,16 @@ export function createEmployee(salary: number | string): Director | Teacher {
   }
   return new Director();
 }
+
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
