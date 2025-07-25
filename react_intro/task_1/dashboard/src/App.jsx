117b1728c3394f979d560b54051aa4ab9f52b086
fchavonet <fchavonet@mail.fr>
2025-07-25 14:36:57 +0200

    Create a notification function (in task_1 folder).

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,30 @@
+import holbertonLogo from './assets/holberton-logo.jpg'
+import './App.css'
+import { getCurrentYear, getFooterCopy } from './utils'
+import Notifications from './Notifications';
+
+function App() {
+
+  return (
+    <>
+      <div className='root-notifications'>
+        <Notifications />
+      </div>
+
+      <div className='App-header'>
+        <img className='holberton-logo' src={holbertonLogo} alt="Holberton logo" />
+        <h1>School Dashboard</h1>
+      </div>
+
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className='App-footer'>
+        <p>Copyright {getCurrentYear()} - {getFooterCopy()}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
