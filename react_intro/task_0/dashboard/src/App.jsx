05effc758b4d1684c78c82fbbaae3818204e486c
fchavonet <fchavonet@mail.fr>
2025-07-25 13:18:19 +0200

    Create a basic React application.

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,24 @@
+import holbertonLogo from './assets/holberton-logo.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App-header'>
+        <img className='holberton-logo' src={holbertonLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className='App-footer'>
+        <p>Copyright 2025 - holberton School</p>
+      </div>
+    </>
+  )
+}
+
+export default App
