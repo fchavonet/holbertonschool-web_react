4e80311e9f21266611724610244fa6c0e7360dea
fchavonet <fchavonet@mail.fr>
2025-07-27 12:32:07 +0200

    Fix: test.

--- react_intro/task_5/dashboard/src/Notifications/Notifications.spec.js
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.spec.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import Notifications from './Notifications';
+import Notifications from '../Notifications/Notifications';
 
 test('Renders the notifications title', () => {
     render(<Notifications />);
