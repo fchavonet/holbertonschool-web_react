de14a365cdda351c8fd61f523d817fd0fdfa8bf6
fchavonet <fchavonet@mail.fr>
2025-07-29 13:38:12 +0200

    Create "CourseList" and "CourseListRow" components with conditional rendering based on login state.

---
+++ react_props/task_4/dashboard/src/CourseList/CourseListRow.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function CourseListRow({ isHeader = false, textFirstCell = "", textSecondCell = null }) {
+    if (isHeader === true) {
+        if (textSecondCell === null) {
+            return (
+                <tr>
+                    <th colSpan="2">{textFirstCell}</th>
+                </tr>
+            );
+        } else {
+            return (
+                <tr>
+                    <th>{textFirstCell}</th>
+                    <th>{textSecondCell}</th>
+                </tr>
+            );
+        }
+    } else {
+        if (textSecondCell === null) {
+            return (
+                <tr>
+                    <td colSpan="2" style={{ textAlign: 'center' }}>{textFirstCell}</td>
+                </tr>
+            );
+        } else {
+            return (
+                <tr>
+                    <td>{textFirstCell}</td>
+                    <td>{textSecondCell}</td>
+                </tr>
+            );
+        }
+    }
+}
+
+export default CourseListRow;
