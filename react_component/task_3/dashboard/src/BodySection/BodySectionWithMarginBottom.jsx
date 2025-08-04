a2ea0989afa8239fbeedc2ddf2c8ef8d3b61abea
fchavonet <fchavonet@mail.fr>
2025-08-04 14:27:19 +0200

    Fix: "}" position.

--- react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
+++ react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import BodySection from './BodySection';
 import './BodySectionWithMarginBottom.spec';
 
-function BodySectionWithMarginBottom({ title }, children) {
+function BodySectionWithMarginBottom({ title, children }) {
     return (
         <div className='bodySectionWithMargin'>
             <BodySection title={title}>
