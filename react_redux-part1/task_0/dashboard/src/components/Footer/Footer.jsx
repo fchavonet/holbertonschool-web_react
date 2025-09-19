ee84333e3f16fc4176029632aad657f5768a9661
fchavonet <fchavonet@mail.fr>
2025-09-19 19:38:34 +0200

    Fix: remove second logout (ask in task before, don't understand this fucking project...) !

--- react_redux-part1/task_0/dashboard/src/components/Footer/Footer.jsx
+++ react_redux-part1/task_0/dashboard/src/components/Footer/Footer.jsx
@@ -4,11 +4,6 @@ import { getCurrentYear, getFooterCopy } from '../../utils/utils';
 function Footer({ user, logOut }) {
   return (
     <footer className='App-footer' style={{ textAlign: 'center' }}>
-      {user.isLoggedIn && (
-        <div style={{ marginBottom: '0.5rem' }}>
-          <p><a href="#" onClick={(e) => { e.preventDefault(); logOut(); }} aria-label="Logout">Logout</a></p>
-        </div>
-      )}
       <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
     </footer>
   );
